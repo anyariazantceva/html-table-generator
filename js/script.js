78fe4222a1e9e0d32eb36c7a3c5f13a0e7587d30
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-01-21 10:20:58 +0200

    Basic logic structure of script has written

---
+++ js/script.js
@@ -0,0 +1,117 @@
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addStyles();
+
+});
+
+// common function to create a table
+function createTable () {
+    let tableObject = generateObject();
+    let table = document.createElement('table');
+    let tbody = document.createElement('tbody');
+    let thead = document.createElement('thead');
+    let firstRow = document.createElement('tr');
+    for(let i = 0; i < tableObject.columns; i++) {
+        let th = document.createElement('th');
+        th.textContent = 'value';
+        firstRow.append(th);
+    }
+
+    thead.append(firstRow);
+    for(let i = 0; i<tableObject.rows - 1; i++) {
+        let tr = document.createElement('tr');
+        for(let i = 0; i < tableObject.columns; i++) {
+            let td = document.createElement('td');
+            td.textContent = 'value';
+            tr.append(td);
+        }
+
+        tbody.append(tr);
+
+    }
+    table.append(thead, tbody);
+    return table
+}
+
+// adding all settings to created table
+function addStyles () {
+    let tableObject = generateObject();
+    let container = document.querySelector('.page__table');
+    let table = createTable();
+    table.style.width = `${tableObject.tableWidth}%`;
+    table.style.border = `${tableObject.borderWidth}px solid ${tableObject.tableBorder}`;
+    table.style.borderCollapse = `${tableObject.checked ? 'collapse' : 'separated'}`;
+    table.firstChild.style.backgroundColor = `${tableObject.tableHead}`;
+    table.lastChild.style.backgroundColor = `${tableObject.tableBody}`;
+
+    table.style.fontFamily = `${tableObject.fontFamily} sans-serif`;
+    table.style.fontWeight = `${tableObject.fontWeight}`;
+    table.style.fontSize = `${tableObject.fontSize}px`;
+    container.append(table);
+}
+
+function generateObject () {
+    let table = {
+        rows: getNumber('#table-rows'),
+        columns: getNumber('#table-columns'),
+        tableWidth: getWidth('#table-width'),
+        borderWidth: getWidth('#table-border'),
+        checked: setCollapse(),
+        tableBg: getColorsForTable('#table-color'),
+        tableHead: getColorsForTable('#table-head-color'),
+        tableBody: getColorsForTable('#table-body-color'),
+        tableBorder: getColorsForTable('#table-border-color'),
+        tableFont: getColorsForTable('#table-font'),
+        fontFamily: getFontFamily(),
+        fontWeight: getFontSettings('#font-weight'),
+        fontSize: getFontSettings('#font-size')
+
+    };
+    return table
+}
+// get number of rows and columns
+function getNumber (prop) {
+    let number = parseInt(document.querySelector(prop).value);
+    return number
+}
+
+// get width
+function getWidth(prop) {
+    let width = parseInt(document.querySelector(prop).value);
+    return width
+
+}
+
+// function setting collapse attribute to the table
+function setCollapse () {
+    let checkbox = document.querySelector('#border-collapse');
+    if(checkbox.checked !== true) {
+        checkbox.setAttribute('checked', 'true');
+    }
+    return checkbox.checked;
+}
+
+// function to get all the color settings
+function getColorsForTable (tablePart) {
+    let color = document.querySelector(tablePart).value;
+    return color
+}
+
+// get font family
+function getFontFamily () {
+    let select = document.querySelector('#font-type');
+    let font = select.options[select.selectedIndex].value;
+    return font
+}
+
+// function get font settings
+function getFontSettings (prop) {
+    let num = parseInt(document.querySelector(prop).value);
+    return num
+}
+
+// clearing a form
+function clearForm () {
+
+}
